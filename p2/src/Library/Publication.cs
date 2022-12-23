d796cb788a7c2e2782f8be40981c59d08dd87d04
Leandro-Casaretto <leandrodcasaretto@gmail.com>
2022-12-23 08:48:12 -0300

    Desafío 2 Terminado

--- p2/src/Library/Publication.cs
+++ p2/src/Library/Publication.cs
@@ -33,5 +33,23 @@ namespace Ucu.Poo.Defense
         {
             this.items.Remove(item);
         }
+
+        /// <summary>
+        /// Como se usa la directiva System.Text, podemos usar un StringBuilder
+        /// /// </summary>
+        
+        
+        public string AsText()
+        {
+            
+            StringBuilder text = new StringBuilder();
+            foreach(PublicationItem item in items)
+            {
+                text.Append(item.Material.Name);
+                text.Append(item.Quantity.ToString());
+                text.Append(item.Price.ToString());
+            }
+            return text.ToString();
+        }
     }
 }
